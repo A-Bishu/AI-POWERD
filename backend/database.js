a963846a7ba2b4f7ce057bc8b642fa947da9ca19
A-Bishu <bishuk001@gmail.com>
2024-11-05 18:26:58 -0600

    updated database configuration

--- backend/database.js
+++ backend/database.js
@@ -4,10 +4,20 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+const sequelize = new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASS, 
+    {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
 });
 
 module.exports = { sequelize, DataTypes };
