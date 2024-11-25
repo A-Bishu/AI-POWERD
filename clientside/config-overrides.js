2e056adb2cd49e35b13d748fb1befadddfef9f95
A-Bishu <bishuk001@gmail.com>
2024-11-24 18:44:31 -0600

    delete the workflow

--- clientside/config-overrides.js
+++
@@ -1,9 +0,0 @@
-const { override, addBabelPlugins } = require('customize-cra');
-
-module.exports = override(
-  ...addBabelPlugins(
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-private-methods',
-    '@babel/plugin-proposal-private-property-in-object'
-  )
-);
